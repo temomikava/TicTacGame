78cf1c88eda60425a9095ab075783563261b7150
Temur Mikava <tmikava@europebet.com>
2022-05-27 20:43:47 +0400

    Created Game Logic

---
+++ GameLibrary/GameResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public class GameResult
+    {
+        public Player Winner { get; set; }
+        public WinInfo WinInfo { get; set; }
+    }
+}
