00803aabc5e442f365419b60b08693cca76ab63c
Temur Mikava <tmikava@europebet.com>
2022-07-28 10:32:58 +0400

    comments

--- WebAPI/SignalR/GameHub.cs
+++ WebAPI/SignalR/GameHub.cs
@@ -147,102 +147,102 @@ namespace WebAPI.SignalR
 
         }
 
-        public async Task MakeMove(int gameId,int r , int c)
-        {
-            //int gameId = request.GameId;
-            //int r = request.Row;
-            //int c = request.Column;
-            int callerId = int.Parse(Context.User.Claims.First(x => x.Type == ClaimTypes.Name).Value);
+        //public async Task MakeMove(int gameId,int r , int c)
+        //{
+        //    //int gameId = request.GameId;
+        //    //int r = request.Row;
+        //    //int c = request.Column;
+        //    int callerId = int.Parse(Context.User.Claims.First(x => x.Type == ClaimTypes.Name).Value);
 
-            mainGame = _connection.GetGameByID(gameId);
-            mainMatch = _connection.GetActiveMatch(gameId);
+        //    mainGame = _connection.GetGameByID(gameId);
+        //    mainMatch = _connection.GetActiveMatch(gameId);
 
-            if (mainMatch == null)
-            {
-                await Clients.Caller.SendAsync("nextturn", -1, "match is not started");
-                return;
-            }
+        //    if (mainMatch == null)
+        //    {
+        //        await Clients.Caller.SendAsync("nextturn", -1, "match is not started");
+        //        return;
+        //    }
 
-            mainMatch.PlayerOne = new Player { Id = mainGame.PlayerOne.Id, UserName = mainGame.PlayerOne.UserName };
-            mainMatch.PlayerTwo = new Player { Id = mainGame.PlayerTwo.Id, UserName = mainGame.PlayerTwo.UserName };
-            mainMatch.GameGrid = _connection.FillGrid(mainMatch);
-            if (mainMatch.CurrentPlayerId != callerId)
-            {
-                await Clients.Caller.SendAsync("nextturn", -1, "wait for your turn");
-                return;
+        //    mainMatch.PlayerOne = new Player { Id = mainGame.PlayerOne.Id, UserName = mainGame.PlayerOne.UserName };
+        //    mainMatch.PlayerTwo = new Player { Id = mainGame.PlayerTwo.Id, UserName = mainGame.PlayerTwo.UserName };
+        //    mainMatch.GameGrid = _connection.FillGrid(mainMatch);
+        //    if (mainMatch.CurrentPlayerId != callerId)
+        //    {
+        //        await Clients.Caller.SendAsync("nextturn", -1, "wait for your turn");
+        //        return;
 
-            }
+        //    }
 
-            mainMatch.CurrentPlayer = mainMatch.CurrentPlayerId == mainGame.PlayerOne.Id ? Mark.X : Mark.O;
-            var makeMove = mainMatch.MakeMove(r, c);
-            if (makeMove.ErrorCode != 1)
-            {
-                await Clients.Caller.SendAsync("nextturn", -1, makeMove.ErrorMessage);
-                return;
-            }
-            if (mainMatch.MatchOver != true)
-            {
-                var caller = callerId == mainMatch.PlayerOne.Id ? mainMatch.PlayerOne : mainMatch.PlayerTwo;
-                var opponent = caller == mainMatch.PlayerOne ? mainMatch.PlayerTwo : mainMatch.PlayerOne;
-                await Clients.Caller.SendAsync("nextturn", 1, opponent.UserName + "`s turn");
-                await Clients.User(opponent.Id.ToString()).SendAsync("nextturn", 1, "your turn");
-                _connection.MakeMove(mainMatch, r, c);
-                return;
-            }
-           MatchEnd();
-        }     
-        private async void MatchEnd()
-        {
-            List<string> ids = new List<string> { mainMatch.PlayerOne.Id.ToString(), mainMatch.PlayerTwo.Id.ToString() };
+        //    mainMatch.CurrentPlayer = mainMatch.CurrentPlayerId == mainGame.PlayerOne.Id ? Mark.X : Mark.O;
+        //    var makeMove = mainMatch.MakeMove(r, c);
+        //    if (makeMove.ErrorCode != 1)
+        //    {
+        //        await Clients.Caller.SendAsync("nextturn", -1, makeMove.ErrorMessage);
+        //        return;
+        //    }
+        //    if (mainMatch.MatchOver != true)
+        //    {
+        //        var caller = callerId == mainMatch.PlayerOne.Id ? mainMatch.PlayerOne : mainMatch.PlayerTwo;
+        //        var opponent = caller == mainMatch.PlayerOne ? mainMatch.PlayerTwo : mainMatch.PlayerOne;
+        //        await Clients.Caller.SendAsync("nextturn", 1, opponent.UserName + "`s turn");
+        //        await Clients.User(opponent.Id.ToString()).SendAsync("nextturn", 1, "your turn");
+        //        _connection.MakeMove(mainMatch, r, c);
+        //        return;
+        //    }
+        //   MatchEnd();
+        //}     
+        //private async void MatchEnd()
+        //{
+        //    List<string> ids = new List<string> { mainMatch.PlayerOne.Id.ToString(), mainMatch.PlayerTwo.Id.ToString() };
 
-            Player winner = new Player();
-            Player loser = new Player();
-            if (mainMatch.MatchResult.Winner != Mark.None)
-            {
-                winner = mainMatch.CurrentPlayerId == mainMatch.PlayerOne.Id ? mainMatch.PlayerOne : mainMatch.PlayerTwo;
-                loser = winner == mainMatch.PlayerOne ? mainMatch.PlayerTwo : mainMatch.PlayerOne;
+        //    Player winner = new Player();
+        //    Player loser = new Player();
+        //    if (mainMatch.MatchResult.Winner != Mark.None)
+        //    {
+        //        winner = mainMatch.CurrentPlayerId == mainMatch.PlayerOne.Id ? mainMatch.PlayerOne : mainMatch.PlayerTwo;
+        //        loser = winner == mainMatch.PlayerOne ? mainMatch.PlayerTwo : mainMatch.PlayerOne;
 
-                if (winner == mainMatch.PlayerOne)
-                {
-                    mainMatch.PlayerOneScore = ++mainGame.PlayerOneScore;
-                    mainMatch.PlayerTwoScore = mainGame.PlayerTwoScore;
-                    mainMatch.WinnerId = mainMatch.PlayerOne.Id;
-                }
-                else
-                {
-                    mainMatch.PlayerTwoScore = ++mainGame.PlayerTwoScore;
-                    mainMatch.PlayerOneScore = mainGame.PlayerOneScore;
-                    mainMatch.WinnerId = mainMatch.PlayerTwo.Id;
-                }
+        //        if (winner == mainMatch.PlayerOne)
+        //        {
+        //            mainMatch.PlayerOneScore = ++mainGame.PlayerOneScore;
+        //            mainMatch.PlayerTwoScore = mainGame.PlayerTwoScore;
+        //            mainMatch.WinnerId = mainMatch.PlayerOne.Id;
+        //        }
+        //        else
+        //        {
+        //            mainMatch.PlayerTwoScore = ++mainGame.PlayerTwoScore;
+        //            mainMatch.PlayerOneScore = mainGame.PlayerOneScore;
+        //            mainMatch.WinnerId = mainMatch.PlayerTwo.Id;
+        //        }
 
-                _connection.MatchEnd(mainMatch);
-            }
-            else
-            {
-                mainMatch.PlayerOneScore = mainGame.PlayerOneScore;
-                mainMatch.PlayerTwoScore = mainGame.PlayerTwoScore;
-                mainMatch.WinnerId = -1;
-                _connection.MatchEnd(mainMatch);
-                await Clients.Users(ids).SendAsync("matchend", mainMatch.PlayerOneScore, mainMatch.PlayerTwoScore, "it is a tie");
-                await MatchStart(mainGame.Id);
-                return;
-            }
-            if (mainGame.TargetScore != mainMatch.PlayerOneScore && mainGame.TargetScore != mainMatch.PlayerTwoScore)
-            {
+        //        _connection.MatchEnd(mainMatch);
+        //    }
+        //    else
+        //    {
+        //        mainMatch.PlayerOneScore = mainGame.PlayerOneScore;
+        //        mainMatch.PlayerTwoScore = mainGame.PlayerTwoScore;
+        //        mainMatch.WinnerId = -1;
+        //        _connection.MatchEnd(mainMatch);
+        //        await Clients.Users(ids).SendAsync("matchend", mainMatch.PlayerOneScore, mainMatch.PlayerTwoScore, "it is a tie");
+        //        await MatchStart(mainGame.Id);
+        //        return;
+        //    }
+        //    if (mainGame.TargetScore != mainMatch.PlayerOneScore && mainGame.TargetScore != mainMatch.PlayerTwoScore)
+        //    {
 
-                await Clients.User(winner.Id.ToString()).SendAsync("matchend", mainGame.PlayerOneScore, mainGame.PlayerTwoScore, "you win the match!");
-                await Clients.User(loser.Id.ToString()).SendAsync("matchend", mainGame.PlayerOneScore, mainGame.PlayerTwoScore, "you lose the match!");
-                _connection.MatchEnd(mainMatch);
+        //        await Clients.User(winner.Id.ToString()).SendAsync("matchend", mainGame.PlayerOneScore, mainGame.PlayerTwoScore, "you win the match!");
+        //        await Clients.User(loser.Id.ToString()).SendAsync("matchend", mainGame.PlayerOneScore, mainGame.PlayerTwoScore, "you lose the match!");
+        //        _connection.MatchEnd(mainMatch);
 
-                await MatchStart(mainGame.Id);            
-            }
-            else
-            {
-                _connection.MatchEnd(mainMatch);
-                mainGame.Winner_Player_id = mainMatch.WinnerId;
-                mainGame.StateId = (int)StateType.Finished;
-                _connection.GameEnd(mainGame);
-            }
-        }
+        //        await MatchStart(mainGame.Id);            
+        //    }
+        //    else
+        //    {
+        //        _connection.MatchEnd(mainMatch);
+        //        mainGame.Winner_Player_id = mainMatch.WinnerId;
+        //        mainGame.StateId = (int)StateType.Finished;
+        //        _connection.GameEnd(mainGame);
+        //    }
+        //}
     }
 }
